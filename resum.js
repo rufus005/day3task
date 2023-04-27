45b04657da36bda3344e24faad9ad3fde5c0a42b
rufus005 <rufussam.orion@gmail.com>
2023-04-27 17:17:04 +0530

    day 3 task

---
+++ resum.js
@@ -0,0 +1,65 @@
+let resume = {
+    "name":"Rufus",
+    "fatherName":"samson",
+    "mobile":"6369699399",
+    "education":[
+        {
+            "type1":"sslc",
+            "school1":"Govt.hr.sec.school",
+            "percentage1":"65"
+
+        },
+        {
+            "type2":"hsc",
+            "school2":"Govt.hr.sec.school",
+            "percentage2":"70"
+        },
+        {
+            "type3":"UG",
+            "college3":"KMG",
+            "percentage3":"57"
+        }
+        ],
+        "programmingskills":["basic javascript"]
+
+    }
+
+    console.log(resume)
+
+
+ // output
+
+// {name: 'Rufus', fatherName: 'samson', mobile: '6369699399', education: Array(3), programmingskills: Array(1)}
+// education
+// : 
+// Array(3)
+// 0
+// : 
+// {type1: 'sslc', school1: 'Govt.hr.sec.school', percentage1: '65'}
+// 1
+// : 
+// {type2: 'hsc', school2: 'Govt.hr.sec.school', percentage2: '70'}
+// 2
+// : 
+// {type3: 'UG', college3: 'KMG', percentage3: '57'}
+// length
+// : 
+// 3
+// [[Prototype]]
+// : 
+// Array(0)
+// fatherName
+// : 
+// "samson"
+// mobile
+// : 
+// "6369699399"
+// name
+// : 
+// "Rufus"
+// programmingskills
+// : 
+// ['basic javascript']
+// [[Prototype]]
+// : 
+// Object
